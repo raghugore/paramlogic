8ea6a3342af0b79da1a882b25776a0c40b540f14
Aishwarya Kashid <aishukashid@gmail.com>
2021-09-22 19:02:24 +0530

    Step 1

--- aishwarya/Task.js
+++ aishwarya/Task.js
@@ -3,9 +3,9 @@
 let d = 22;
 let b = 4;
 
-function add(num1, num2) {
-  return num1 + num2;
+function add(num3, num2) {
+  return num3 + num2;
 }
 
-console.log(add(a, b));
+console.log(add(a, b, c));
 console.log(c,d);
